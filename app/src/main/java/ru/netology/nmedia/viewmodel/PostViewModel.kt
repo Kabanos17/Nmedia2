2de20537f03dd50cec86673111e0777ec78e5855
Kabanos17 <nmfilimonzavdv@gmail.com>
2025-02-27 23:49:33 +0500

    InComm2

--- app/src/main/java/ru/netology/nmedia/viewmodel/PostViewModel.kt
+++ app/src/main/java/ru/netology/nmedia/viewmodel/PostViewModel.kt
@@ -1,11 +1,35 @@
 package ru.netology.nmedia.viewmodel
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import ru.netology.nmedia.repository.PostRepository
-import ru.netology.nmedia.repository.PostRepositoryInMemoryImpl
+import ru.netology.nmedia.dto.Post
+
 
 class PostViewModel : ViewModel() {
-    private val repository: PostRepository = PostRepositoryInMemoryImpl()
-    val data = repository.get()
-    fun like() = repository.like()
+    private val _data = MutableLiveData<Post>()
+    val data: LiveData<Post> = _data
+
+    init {
+        _data.value = Post(
+            id = 1, author = "Нетология. Университет интернет-профессий будущего",
+            content = "Привет, это новая Нетология! Когда-то Нетология начиналась с интенсивов по онлайн-маркетингу. Затем появились курсы по дизайну, разработке, аналитике и управлению. Мы растём сами и помогаем расти студентам: от новичков до уверенных профессионалов. Но самое важное остаётся с нами: мы верим, что в каждом уже есть сила, которая заставляет хотеть больше, целиться выше, бежать быстрее. Наша миссия — помочь встать на путь роста и начать цепочку перемен → http://netolo.gy/fyb",
+            published = "21 мая в 18:36"
+        )
+    }
+
+    fun like() {
+        _data.value?.let { post ->
+            post.likedByMe = !post.likedByMe
+            post.likesCount += if (post.likedByMe) 1 else -1
+            _data.value = post
+        }
+    }
+
+    fun share() {
+        _data.value?.let { post ->
+            post.sharesCount += 1
+            _data.value = post
+        }
+    }
 }
